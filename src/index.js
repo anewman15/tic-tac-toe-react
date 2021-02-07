25f27a87085a0bfc393cdba53fa136b59b7381d3
Abdullah Numan <anewman15@hotmail.com>
2021-02-07 15:02:24 +0600

    Add constructor with state, move value to state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
       <button className="square" onClick={() => alert('click')}>
