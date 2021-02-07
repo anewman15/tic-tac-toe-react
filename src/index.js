595d3d1ac10a3d33dcf0bec6dd1d0f136bd7d5f8
Abdullah Numan <anewman15@hotmail.com>
2021-02-07 15:03:27 +0600

    Replace props.value with state.value, update state with onClick

--- src/index.js
+++ src/index.js
@@ -11,8 +11,11 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
