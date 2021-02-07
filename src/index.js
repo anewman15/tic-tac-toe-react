78d5fc7eb5b569f025f1f7bd58fad0a7e7fe1853
Abdullah Numan <anewman15@hotmail.com>
2021-02-07 14:57:27 +0600

    Add click event to Square buttons

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
