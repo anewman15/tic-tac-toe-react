681d6baf20205640256e761cda75532a9d9a5820
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 14:25:15 +0600

    Change props to Square component to take value from state, add click lisclick listener

--- src/index.js
+++ src/index.js
@@ -28,8 +28,14 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
