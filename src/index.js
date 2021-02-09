2d3f4b61979a4108ae42c4e4a339e92f42c4a346
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 14:30:54 +0600

    Remove constructor and state from Square component, replace with props

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
